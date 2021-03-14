74214bd991e2acc6bcace4d2be8a5f9688cba253
a-abokhssan <a.abokhssan@gmail.com>
2021-03-15 00:01:21 +0300

    feat: check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) return false;
+
+    let stack = [];
+    let dict = bracketsConfig.reduce(
+        (acc, rec) => ({ ...acc, [rec[0]]: rec[1] }),
+        {}
+    );
+    let dict2 = Object.keys(dict).reduce((acc, rec) => {
+        return { ...acc, [dict[rec]]: rec };
+    }, {});
+
+    for (let i = 0; i < str.length; i += 1) {
+        let symb = str[i];
+
+        if (typeof dict[symb] !== "undefined") {
+            if (dict[symb] === symb && stack.indexOf(symb) >= 0) {
+                let openingSymbol = stack.pop();
+                if (dict[openingSymbol] !== symb) {
+                    return false;
+                }
+                continue;
+            }
+            stack.push(symb);
+            continue;
+        }
+
+        if (typeof dict2[symb] !== "undefined") {
+            const openingSymbol = stack.pop();
+            if (dict[openingSymbol] !== symb) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
